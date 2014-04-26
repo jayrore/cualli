a9541dc1c6359781949767bc9581c43609a79974
jayro <jayrore@hotmail.com>
2014-04-26 15:47:21 -0500

    gregamos el formulario con ajax
    hasta este punto no funcionan los submit

---
+++ includes/valida_rfc.php
@@ -0,0 +1,31 @@
+<?php
+	session_start();
+	
+	$msg = array("found"=> false);
+
+/*	$conexion=mysql_connect("127.0.0.1","root","123");
+	if (!$conexion) {
+    	echo "hubo un problema en la conexion";
+    	exit;
+	}
+
+	$bd = mysql_select_db("gaslight",$conexion);
+	if (!$bd) {
+    	echo "no se puedo conectar a la base de datos";
+    	exit;
+    }	
+
+	$query=mysql_query("select id_cliente from clientes where rfc='$_REQUEST[rfc]' or id_cliente='$_REQUEST[rfc]' ",$conexion);
+	if (!$query) {
+    	echo "no se puedo conectar a la base de datos";
+    	exit;
+    }	
+	if ($row=mysql_fetch_array($query))
+		{			
+		$msg['id_cliente'] = $row['id_cliente'];
+		$msg['found'] = true;
+		}*/	
+		$msg['id_cliente'] = "dfkjhfjshfk";
+		$msg['found'] = true;
+$jsonMsg = json_encode($msg, true);
+echo $jsonMsg;
